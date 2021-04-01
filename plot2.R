6c454f5418b9eb9a33b9cc8f307b76cf9dec4cff
Nikhil Gupta <gupta.nikhil29@yahoo.in>
2021-04-02 01:04:30 +0530

    Added files for Coursera EDA PGA 1

---
+++ plot2.R
@@ -0,0 +1,9 @@
+dat <- "household_power_consumption.txt"
+data <- read.table(dat, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+ReqDat <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(ReqDat$Date, ReqDat$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GAP <- as.numeric(ReqDat$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,28 @@
+dat <- "household_power_consumption.txt"
+data <- read.table(dat, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+ReqDat <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(ReqDat$Date, ReqDat$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GAP <- as.numeric(ReqDat$Global_active_power)
+GRP <- as.numeric(ReqDat$Global_reactive_power)
+volt <- as.numeric(ReqDat$Voltage)
+subMetering1 <- as.numeric(ReqDat$Sub_metering_1)
+subMetering2 <- as.numeric(ReqDat$Sub_metering_2)
+subMetering3 <- as.numeric(ReqDat$Sub_metering_3)
+
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, GAP, type="l", xlab="", ylab="Global Active Power", cex=0.2, lwd = 0.1)
+
+plot(datetime, volt, type="l", xlab="datetime", ylab="voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, GRP, type="l", xlab="datetime", ylab="Global_reactive_power", lwd = 0.1)
+
+dev.off()
